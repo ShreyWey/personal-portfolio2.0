7b15da61e9da96f91249b16d06ed92de33971a7c
ShreyWey <109689940+ShreyWey@users.noreply.github.com>
2024-09-10 02:20:08 -0500

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,13 +23,13 @@ const About = () => {
             <article className='about__card'>
               <FaMedal className='about__icon'/>
               <h5>Experience</h5>
-              <small>4+ years of coding</small>
+              <small>10+ years of coding</small>
             </article>
 
             <article className='about__card'>
               <MdSchool className='about__icon'/>
               <h5>Education</h5>
-              <small>Sophmore at TAMU</small>
+              <small>Junior at TAMU</small>
             </article>
 
             <article className='about__card'>
@@ -41,7 +41,7 @@ const About = () => {
 
           <p>
           
-          I am a versatile computer engineer proficient in delivering both software and hardware solutions. My portfolio includes the creation of various software applications, including the one you are currently utilizing. Beyond coding, I am a member of SAE Formula E team, where we design and compete with electric racing cars on an international level. This involvement has granted me valuable automotive experience, particularly in the realm of electronics and data acquisition. I am also now capable of using industry-standard software like KiKad, Altium, RapidHarness, and Race Studio. I also hold certifications in R and Python from the High-Performance Research Computing program at Texas A&M University. This certification has not only expanded my expertise but also provided me with access to and experience in utilizing HPRC clusters.
+          I am a versatile electronics systems engineering proficient in delivering both software and hardware solutions. My portfolio includes the creation of various software applications, including the one you are currently utilizing. Beyond coding, I am a member of SAE Formula E team, where we design and compete with electric racing cars on an international level. This involvement has granted me valuable automotive experience, particularly in the realm of electronics and data acquisition. I am also now capable of using industry-standard software like KiKad, Altium, RapidHarness, and Race Studio. I also hold certifications in R and Python from the High-Performance Research Computing program at Texas A&M University. This certification has not only expanded my expertise but also provided me with access to and experience in utilizing HPRC clusters.
           </p>
 
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
